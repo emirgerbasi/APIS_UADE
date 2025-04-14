6677481c030c8edf89076d5a6f8b6c3f2c0b09e0
Arthur <acallomamani@uade.edu.ar>
2025-04-14 17:01:53 -0300

    update details review

--- src/layouts/ReviewCard.jsx
+++ src/layouts/ReviewCard.jsx
@@ -4,7 +4,7 @@ const ReviewCard = (props) => {
   return (
     <div className=" w-full md:w-1/3 bg-gradient-to-br from-gray-700 to-gray-500 border-2 border-gray-400 md:border-none p-5 rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
       <div>
-        <p className="text-white">
+        <p style={{color: '#F5F5F7'}} className="font-sans">
           {props.review}
         </p>
       </div>
@@ -15,7 +15,7 @@ const ReviewCard = (props) => {
           src={props.img} 
           alt="img" 
         />
-        <h3 className="font-semibold text-white">{props.name}</h3>
+        <h3 className="font-semibold text-black">{props.name}</h3>
       </div>
     </div>
   );
